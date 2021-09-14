53f2f1c033071f7900a46c08686a1a4051a21003
Guilherme Fernandes <guifes@gmail.com>
2021-09-14 13:48:12 -0300

    Adding missing functions from macro utils and ArrayExtension

--- source/guifes/extension/ArrayExtension.hx
+++ source/guifes/extension/ArrayExtension.hx
@@ -4,10 +4,21 @@ class ArrayExtension
 {
 	static public function has<T>(self: Array<T>, find: T -> Bool): Bool
 	{
-        for(item in self)
-            if(find(item))
-                return true;
+		for(item in self)
+		if(find(item))
+				return true;
 
-        return false;
+		return false;
+	}
+
+	/**
+	 * Returns the last element of an array or `null` if the array is `null` / empty.
+	 */
+	 public static function last<T>(array: Array<T>): Null<T>
+	{
+		if (array == null || array.length == 0)
+			return null;
+
+		return array[array.length - 1];
 	}
 }
--- source/guifes/macro/AssetPathsCatalog.hx
+++ source/guifes/macro/AssetPathsCatalog.hx
@@ -33,4 +33,68 @@ class AssetPathsCatalog
         }
         return fields;
     }
+    static function getFileReferences(directory: String, subDirectories: Bool = false, ?filterExtensions: Array<String>, ?rename: String->String): Array<FileReference>
+    {
+        var fileReferences:Array<FileReference> = [];
+        var resolvedPath = #if (ios || tvos) "../assets/" + directory #else directory #end;
+        var directoryInfo = FileSystem.readDirectory(resolvedPath);
+        for (name in directoryInfo)
+        {
+            if (!FileSystem.isDirectory(resolvedPath + name))
+            {
+                // ignore invisible files
+                if (name.startsWith("."))
+                    continue;
+
+                if (filterExtensions != null)
+                {
+                    var extension:String = name.split(".").last(); // get the last string with a dot before it
+                    if (!filterExtensions.contains(extension))
+                        continue;
+                }
+
+                var reference = FileReference.fromPath(directory + name, rename);
+                if (reference != null)
+                    fileReferences.push(reference);
+            }
+            else if (subDirectories)
+            {
+                fileReferences = fileReferences.concat(getFileReferences(directory + name + "/", true, filterExtensions, rename));
+            }
+        }
+
+        return fileReferences;
+    }
+}
+
+private class FileReference
+{
+    private static var validIdentifierPattern = ~/^[_A-Za-z]\w*$/;
+
+    public static function fromPath(value:String, ?rename:String->String):Null<FileReference>
+    {
+        // replace some forbidden names to underscores, since variables cannot have these symbols.
+        var name = value.split("/").last();
+
+        if (rename != null)
+        {
+            name = rename(name);
+        }
+
+        name = name.split("-").join("_").split(".").join("__");
+        if (!validIdentifierPattern.match(name)) // #1796
+            return null;
+        return new FileReference(name, value);
+    }
+
+    public var name(default, null):String;
+    public var value(default, null):String;
+    public var documentation(default, null):String;
+
+    function new(name:String, value:String)
+    {
+        this.name = name;
+        this.value = value;
+        this.documentation = "`\"" + value + "\"` (auto generated).";
+    }
 }
