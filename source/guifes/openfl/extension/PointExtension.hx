c5b7ac6728978618ee104b5d65d475dbe93849e5
Guilherme Fernandes <guifes@gmail.com>
2021-11-26 22:27:47 -0300

    Adding fromVector2i to Openfl Point extension

--- source/guifes/openfl/extension/PointExtension.hx
+++ source/guifes/openfl/extension/PointExtension.hx
@@ -1,11 +1,18 @@
 package guifes.openfl.extension;
 
+import haxe.macro.Printer;
 import openfl.geom.Point;
+import guifes.math.Vector2i;
 
 class PointExtension
 {
 	static public function distanceTo(self: Point, target: Point)
 	{
 		return target.subtract(self).length;
+	}
+
+	static public function fromVector2i(self: Class<Point>, source: Vector2i): Point
+	{
+		return new Point(source.x, source.y);
 	}
 }
